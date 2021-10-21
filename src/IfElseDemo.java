d4621cd74c6f76a3226a6901417d2a29d39673e8
amolu <amolujagare@gmail.com>
2021-10-21 16:44:41 +0530

    Initial commit

---
+++ src/IfElseDemo.java
@@ -0,0 +1,15 @@
+public class IfElseDemo {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20 ;
+
+        if(a>b) {
+            System.out.println("a is greater");
+        }
+        else
+        {
+            System.out.println("b is greater");
+        }
+    }
+}
