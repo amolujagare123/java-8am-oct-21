37a7c879fc1a3065d5e49a677a6820ca9965d5ce
amolu <amolujagare@gmail.com>
2021-10-22 11:48:27 +0530

    basics

---
+++ src/WhileDemo.java
@@ -0,0 +1,13 @@
+public class WhileDemo {
+
+    public static void main(String[] args) {
+       // Print 1 to 10 Numbers
+
+       int i = 1;
+       while (i<=10)
+       {
+           System.out.println(i);
+           i = i + 1;
+       }
+    }
+}
---
+++ src/WhileDemo2.java
@@ -0,0 +1,13 @@
+public class WhileDemo2 {
+
+    public static void main(String[] args) {
+       // Print 10 to 1 Numbers
+
+       int i = 10;
+       while (i>=1)
+       {
+           System.out.println(i);
+           i = i - 1;
+       }
+    }
+}
